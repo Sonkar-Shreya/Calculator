1f59420092a0b4833d1d07b128efa39041fabdef
Sonkar_Shreya <sonkarshreya1@gmail.com>
2023-03-29 18:35:06 +0530

    update buttons

--- script.js
+++ script.js
@@ -1,13 +1,91 @@
 const operators = ["+", "-", "*", "%", "/"];
+const container = [
+  {
+    id: "AC",
+  },
+  {
+    id: "+/-",
+  },
+  {
+    id: "%",
+  },
+  {
+    id: "/",
+  },
+  {
+    id: "7",
+  },
+  {
+    id: "8",
+  },
+  {
+    id: "9",
+  },
+  {
+    id: "*",
+  },
+  {
+    id: "4",
+  },
+  {
+    id: "5",
+  },
+  {
+    id: "6",
+  },
+  {
+    id: "-",
+  },
+  {
+    id: "1",
+  },
+  {
+    id: "2",
+  },
+  {
+    id: "3",
+  },
+  {
+    id: "+",
+  },
+  {
+    id: "0",
+  },
+  {
+    id: ",",
+  },
+  {
+    id: "=",
+  },
+];
+
+for (let i of container) {
+  let button = document.createElement("button");
+  button.innerHTML = i.id;
+  let btn = document.getElementById("btnContainer");
+  button.classList.add("btn");
+  button.addEventListener("click", () => {
+    handleClick(i.id);
+  });
+  if (i.id === "0") {
+    button.style.width = "49.6% ";
+  }
+  btn.appendChild(button);
+}
 
 let operand1 = "";
 let operand2 = "";
 let operator;
 let operand = "";
 let display = "";
+let flag = false;
 
 function handleClick(value) {
-  if (!!Number.parseInt(value)) {
+  if (flag) {
+    flag = false;
+    handleClick("AC");
+  }
+  if (!!Number.parseInt(value) || value === "0") {
     operand = operand.concat(value);
     display = display.concat(value);
   } else if (operators.includes(value)) {
@@ -20,6 +98,8 @@ function handleClick(value) {
     operand = "";
     display = display.concat(value);
   } else if (value === "=") {
+    flag = true;
+
     if (operand1 === "") {
       alert("Enter valid operand");
       return;
